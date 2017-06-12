0d1093af31f373d7c4223dd8070e5c537b9b42ae
Roland Kersche <roland.kersche@bitmovin.com>
2017-06-12 09:27:32 +0200

    Updated CreateEncodingWithMP4MuxingOnS3 example

--- src/test/java/com/bitmovin/api/examples/CreateEncodingWithMP4MuxingOnS3.java
+++ src/test/java/com/bitmovin/api/examples/CreateEncodingWithMP4MuxingOnS3.java
@@ -1,6 +1,8 @@
 package com.bitmovin.api.examples;
 
 import com.bitmovin.api.BitmovinApi;
+import com.bitmovin.api.encoding.AclEntry;
+import com.bitmovin.api.encoding.AclPermission;
 import com.bitmovin.api.encoding.EncodingOutput;
 import com.bitmovin.api.encoding.InputStream;
 import com.bitmovin.api.encoding.codecConfigurations.AACAudioConfig;
@@ -13,11 +15,15 @@ import com.bitmovin.api.encoding.encodings.streams.Stream;
 import com.bitmovin.api.encoding.enums.CloudRegion;
 import com.bitmovin.api.encoding.enums.StreamSelectionMode;
 import com.bitmovin.api.encoding.inputs.HttpsInput;
+import com.bitmovin.api.encoding.outputs.Output;
 import com.bitmovin.api.encoding.outputs.S3Output;
 import com.bitmovin.api.encoding.status.Task;
 import com.bitmovin.api.enums.Status;
 import com.bitmovin.api.exceptions.BitmovinApiException;
 import com.bitmovin.api.http.RestException;
+import com.bitmovin.api.webhooks.Webhook;
+import com.bitmovin.api.webhooks.enums.WebhookHttpMethod;
+import com.bitmovin.api.webhooks.enums.WebhookType;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.junit.Test;
 
@@ -34,23 +40,26 @@ import java.util.List;
 public class CreateEncodingWithMP4MuxingOnS3
 {
 
-    private static String ApiKey = "<INSERT_YOUR_APIKEY>";
+    private static String API_KEY = "<INSERT_YOUR_APIKEY>";
 
-    private static CloudRegion cloudRegion = CloudRegion.GOOGLE;
+    private static CloudRegion CLOUD_REGION = CloudRegion.AWS_EU_WEST_1;
     private static String HTTPS_INPUT_HOST = "<INSERT_YOUR_HTTP_HOST>"; // ex.: storage.googleapis.com/
     private static String HTTPS_INPUT_PATH = "<INSERT_YOUR_PATH_TO_INPUT_FILE>";
     private static String S3_OUTPUT_ACCESSKEY = "<INSERT_YOUR_ACCESSKEY>";
     private static String S3_OUTPUT_SECRET_KEY = "<INSERT_YOUR_SECRETKEY>";
     private static String S3_OUTPUT_BUCKET_NAME = "BUCKET_NAME";
     private static String OUTPUT_BASE_PATH = "path/to/your/outputs/" + new Date().getTime();
+    private static String NOTIFICATION_URL = "<INSERT_YOUR_NOTIFICATION_URL>";
+
+    private static BitmovinApi bitmovinApi;
 
     @Test
     public void testEncoding() throws IOException, BitmovinApiException, UnirestException, URISyntaxException, RestException, InterruptedException
     {
-        BitmovinApi bitmovinApi = new BitmovinApi(ApiKey);
+        bitmovinApi = new BitmovinApi(API_KEY);
         Encoding encoding = new Encoding();
         encoding.setName("Encoding JAVA");
-        encoding.setCloudRegion(cloudRegion);
+        encoding.setCloudRegion(CLOUD_REGION);
         encoding = bitmovinApi.encoding.create(encoding);
 
         HttpsInput input = new HttpsInput();
@@ -125,28 +134,38 @@ public class CreateEncodingWithMP4MuxingOnS3
         videoStream240p.setInputStreams(Collections.singleton(inputStreamVideo));
         videoStream240p = bitmovinApi.encoding.stream.addStream(encoding, videoStream240p);
 
+        Stream videoStream360p = new Stream();
+        videoStream360p.setCodecConfigId(videoConfiguration360p.getId());
+        videoStream360p.setInputStreams(Collections.singleton(inputStreamVideo));
+        videoStream360p = bitmovinApi.encoding.stream.addStream(encoding, videoStream360p);
+
+        Stream videoStream480p = new Stream();
+        videoStream480p.setCodecConfigId(videoConfiguration480p.getId());
+        videoStream480p.setInputStreams(Collections.singleton(inputStreamVideo));
+        videoStream480p = bitmovinApi.encoding.stream.addStream(encoding, videoStream480p);
+
+        Stream videoStream720p = new Stream();
+        videoStream720p.setCodecConfigId(videoConfiguration720p.getId());
+        videoStream720p.setInputStreams(Collections.singleton(inputStreamVideo));
+        videoStream720p = bitmovinApi.encoding.stream.addStream(encoding, videoStream720p);
+
+        Stream videoStream1080p = new Stream();
+        videoStream1080p.setCodecConfigId(videoConfiguration1080p.getId());
+        videoStream1080p.setInputStreams(Collections.singleton(inputStreamVideo));
+        videoStream1080p = bitmovinApi.encoding.stream.addStream(encoding, videoStream1080p);
+
         Stream audioStream = new Stream();
         audioStream.setCodecConfigId(aacConfiguration.getId());
         audioStream.setInputStreams(Collections.singleton(inputStreamAudio));
         audioStream = bitmovinApi.encoding.stream.addStream(encoding, audioStream);
 
-        EncodingOutput encodingOutput = new EncodingOutput();
-        encodingOutput.setOutputId(output.getId());
-        encodingOutput.setOutputPath(OUTPUT_BASE_PATH);
-        MP4Muxing mp4Muxing = new MP4Muxing();
-        mp4Muxing.setFilename("test.mp4");
-        mp4Muxing.setOutputs(Collections.singletonList(encodingOutput));
+        this.createMP4Muxing(encoding, output, videoStream240p, audioStream, "video_audio_240p.mp4");
+        this.createMP4Muxing(encoding, output, videoStream360p, audioStream, "video_audio_360p.mp4");
+        this.createMP4Muxing(encoding, output, videoStream480p, audioStream, "video_audio_480p.mp4");
+        this.createMP4Muxing(encoding, output, videoStream720p, audioStream, "video_audio_720p.mp4");
+        this.createMP4Muxing(encoding, output, videoStream1080p, audioStream, "video_audio_1080p.mp4");
 
-        List<MuxingStream> muxingStreams = new ArrayList<>();
-        MuxingStream muxingStreamVideo = new MuxingStream();
-        muxingStreamVideo.setStreamId(videoStream240p.getId());
-        MuxingStream muxingStreamAudio = new MuxingStream();
-        muxingStreamAudio.setStreamId(audioStream.getId());
-        muxingStreams.add(muxingStreamVideo);
-        muxingStreams.add(muxingStreamAudio);
-
-        mp4Muxing.setStreams(muxingStreams);
-        bitmovinApi.encoding.muxing.addMp4MuxingToEncoding(encoding, mp4Muxing);
+        this.createWebHook(encoding);
 
         bitmovinApi.encoding.start(encoding);
 
@@ -160,6 +179,38 @@ public class CreateEncodingWithMP4MuxingOnS3
 
         System.out.println(String.format("Encoding finished with status %s", status.getStatus().toString()));
 
+    }
+
+    private void createWebHook(Encoding encoding) throws URISyntaxException, BitmovinApiException, RestException, UnirestException, IOException
+    {
+        Webhook webhook = new Webhook();
+        webhook.setUrl(NOTIFICATION_URL);
+        webhook.setMethod(WebhookHttpMethod.POST);
+        bitmovinApi.notifications.webhooks.create(webhook, WebhookType.ENCODING_FINISHED, encoding.getId());
+        bitmovinApi.notifications.webhooks.create(webhook, WebhookType.ENCODING_ERROR, encoding.getId());
+    }
+
+    private void createMP4Muxing(Encoding encoding, Output output, Stream videoStream240p, Stream audioStream, String filename) throws BitmovinApiException, IOException, RestException, URISyntaxException, UnirestException
+    {
+        EncodingOutput encodingOutput = new EncodingOutput();
+        encodingOutput.setOutputId(output.getId());
+        encodingOutput.setOutputPath(OUTPUT_BASE_PATH);
+        encodingOutput.setAcl(new ArrayList<AclEntry>()
+        {{
+            this.add(new AclEntry(AclPermission.PUBLIC_READ));
+        }});
+        MP4Muxing mp4Muxing = new MP4Muxing();
+        mp4Muxing.setFilename(filename);
+        mp4Muxing.setOutputs(Collections.singletonList(encodingOutput));
+        List<MuxingStream> muxingStreams = new ArrayList<>();
+        MuxingStream muxingStreamVideo = new MuxingStream();
+        muxingStreamVideo.setStreamId(videoStream240p.getId());
+        MuxingStream muxingStreamAudio = new MuxingStream();
+        muxingStreamAudio.setStreamId(audioStream.getId());
+        muxingStreams.add(muxingStreamVideo);
+        muxingStreams.add(muxingStreamAudio);
+        mp4Muxing.setStreams(muxingStreams);
+        bitmovinApi.encoding.muxing.addMp4MuxingToEncoding(encoding, mp4Muxing);
     }
 
 }
