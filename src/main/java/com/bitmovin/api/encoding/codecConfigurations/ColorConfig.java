d28d3ee84fdf21861380b4bacdec023347971ccf
James Yeh <james.yeh@bitmovin.com>
2018-03-05 17:41:42 -0800

    adding our ColorConfig to our VideoConfiguration

---
+++ src/main/java/com/bitmovin/api/encoding/codecConfigurations/ColorConfig.java
@@ -0,0 +1,106 @@
+package com.bitmovin.api.encoding.codecConfigurations;
+
+import com.bitmovin.api.encoding.codecConfigurations.enums.ChromaLocation;
+import com.bitmovin.api.encoding.codecConfigurations.enums.ColorPrimaries;
+import com.bitmovin.api.encoding.codecConfigurations.enums.ColorRange;
+import com.bitmovin.api.encoding.codecConfigurations.enums.ColorSpace;
+import com.bitmovin.api.encoding.codecConfigurations.enums.ColorTransfer;
+
+/**
+ * Created by Roland Kersche on 27.09.17.
+ */
+public class ColorConfig
+{
+    private Boolean copyChromaLocationFlag;
+    private Boolean copyColorSpaceFlag;
+    private Boolean copyColorPrimariesFlag;
+    private Boolean copyColorRangeFlag;
+    private Boolean copyColorTransferFlag;
+
+    private ChromaLocation chromaLocation;
+    private ColorSpace colorSpace;
+    private ColorPrimaries colorPrimaries;
+    private ColorRange colorRange;
+    private ColorTransfer colorTransfer;
+
+    public Boolean getCopyChromaLocationFlag()
+    {
+        return this.copyChromaLocationFlag;
+    }
+    public void setCopyChromaLocationFlag(Boolean copyChromaLocationFlag)
+    {
+        this.copyChromaLocationFlag = copyChromaLocationFlag;
+    }
+    public Boolean getCopyColorSpaceFlag()
+    {
+        return this.copyColorSpaceFlag;
+    }
+    public void setCopyColorSpaceFlag(Boolean copyColorSpaceFlag)
+    {
+        this.copyColorSpaceFlag = copyColorSpaceFlag;
+    }
+    public Boolean getCopyColorPrimariesFlag()
+    {
+        return this.copyColorPrimariesFlag;
+    }
+    public void setCopyColorPrimariesFlag(Boolean copyColorPrimariesFlag)
+    {
+        this.copyColorPrimariesFlag = copyColorPrimariesFlag;
+    }
+    public Boolean getCopyColorRangeFlag()
+    {
+        return this.copyColorRangeFlag;
+    }
+    public void setCopyColorRangeFlag(Boolean copyColorRangeFlag)
+    {
+        this.copyColorRangeFlag = copyColorRangeFlag;
+    }
+    public Boolean getCopyColorTransferFlag()
+    {
+        return this.copyColorTransferFlag;
+    }
+    public void setCopyColorTransferFlag(Boolean copyColorTransferFlag)
+    {
+        this.copyColorTransferFlag = copyColorTransferFlag;
+    }
+    public ChromaLocation getChromaLocation()
+    {
+        return this.chromaLocation;
+    }
+    public void setChromaLocation(ChromaLocation chromaLocation)
+    {
+        this.chromaLocation = chromaLocation;
+    }
+    public ColorSpace getColorSpace()
+    {
+        return this.colorSpace;
+    }
+    public void setColorSpace(ColorSpace colorSpace)
+    {
+        this.colorSpace = colorSpace;
+    }
+    public ColorPrimaries getColorPrimaries()
+    {
+        return this.colorPrimaries;
+    }
+    public void setColorPrimaries(ColorPrimaries colorPrimaries)
+    {
+        this.colorPrimaries = colorPrimaries;
+    }
+    public ColorRange getColorRange()
+    {
+        return this.colorRange;
+    }
+    public void setColorRange(ColorRange colorRange)
+    {
+        this.colorRange = colorRange;
+    }
+    public ColorTransfer getColorTransfer()
+    {
+        return this.colorTransfer;
+    }
+    public void setColorTransfer(ColorTransfer colorTransfer)
+    {
+        this.colorTransfer = colorTransfer;
+    }
+}
---
+++ src/main/java/com/bitmovin/api/encoding/codecConfigurations/enums/ColorTransfer.java
@@ -0,0 +1,25 @@
+package com.bitmovin.api.encoding.codecConfigurations.enums;
+
+/**
+ * Created by Roland Kersche on 27.09.17.
+ */
+public enum ColorTransfer
+{
+    UNSPECIFIED,
+    BT709,
+    GAMMA22,
+    GAMMA28,
+    SMPTE170M,
+    SMPTE240M,
+    LINEAR,
+    LOG,
+    LOG_SQRT,
+    IEC61966_2_4,
+    BT1361_ECG,
+    IEC61966_2_1,
+    BT2020_10,
+    BT2020_12,
+    SMPTE2084,
+    SMPTE428,
+    ARIB_STD_B67
+}
