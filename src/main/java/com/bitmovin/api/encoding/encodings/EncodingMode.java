47755e3fe991ef1173e00f968b28c89f2976d4dc
Roland Kersche <roland.kersche@bitmovin.com>
2018-07-03 10:46:22 +0200

    Added per title

---
+++ src/main/java/com/bitmovin/api/encoding/encodings/EncodingMode.java
@@ -0,0 +1,8 @@
+package com.bitmovin.api.encoding.encodings;
+
+public enum EncodingMode
+{
+    STANDARD,
+    TWO_PASS,
+    THREE_PASS
+}
---
+++ src/main/java/com/bitmovin/api/encoding/encodings/streams/StreamMode.java
@@ -0,0 +1,8 @@
+package com.bitmovin.api.encoding.encodings.streams;
+
+public enum StreamMode
+{
+    STANDARD,
+    PER_TITLE_TEMPLATE,
+    PER_TITLE_RESULT
+}
