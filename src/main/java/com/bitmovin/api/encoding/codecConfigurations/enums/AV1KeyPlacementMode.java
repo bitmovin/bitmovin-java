b206ddec30bb29cd65c0bacc5ac4ef9cd946d895
alodijutgla <allodi.serrat@bitmovin.com>
2018-08-16 15:06:01 +0200

    Creation of the enum keyPlacementMode for the AV1 codec

---
+++ src/main/java/com/bitmovin/api/encoding/codecConfigurations/enums/AV1KeyPlacementMode.java
@@ -0,0 +1,15 @@
+package com.bitmovin.api.encoding.codecConfigurations.enums;
+
+/**
+ * Created by
+ * Al·lodi Jutgla Serrat [allodi.serrat@bitmovin.com]
+ * on 16.08.18.
+ */
+
+public enum AV1KeyPlacementMode
+{
+    AUTO,
+    FIXED,
+    DISABLED
+
+}
