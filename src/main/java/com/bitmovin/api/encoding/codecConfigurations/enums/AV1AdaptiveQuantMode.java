c2f9cf008a6f97f499561448acf9876418bc17f3
alodijutgla <allodi.serrat@bitmovin.com>
2018-08-16 15:05:25 +0200

    Creation of enum adaptiveQuantMode for the AV1 codec

---
+++ src/main/java/com/bitmovin/api/encoding/codecConfigurations/enums/AV1AdaptiveQuantMode.java
@@ -0,0 +1,16 @@
+package com.bitmovin.api.encoding.codecConfigurations.enums;
+
+/**
+ * Created by
+ * Al·lodi Jutgla Serrat [allodi.serrat@bitmovin.com]
+ * on 16.08.18.
+ */
+
+public enum AV1AdaptiveQuantMode
+{
+    OFF,
+    VARIANCE,
+    COMPLEXITY,
+    CYCLIC_REFRESH,
+    DELTA_QUANT
+}
