a73f0081815c2578cfd8324455e613b42e49fcf6
alodijutgla <allodi.serrat@bitmovin.com>
2018-08-17 08:56:11 +0200

    Remove the header comments and a empty line.

--- src/main/java/com/bitmovin/api/encoding/codecConfigurations/enums/AV1KeyPlacementMode.java
+++ src/main/java/com/bitmovin/api/encoding/codecConfigurations/enums/AV1KeyPlacementMode.java
@@ -1,15 +1,9 @@
 package com.bitmovin.api.encoding.codecConfigurations.enums;
 
-/**
- * Created by
- * Al·lodi Jutgla Serrat [allodi.serrat@bitmovin.com]
- * on 16.08.18.
- */
 
 public enum AV1KeyPlacementMode
 {
     AUTO,
     FIXED,
     DISABLED
-
 }
