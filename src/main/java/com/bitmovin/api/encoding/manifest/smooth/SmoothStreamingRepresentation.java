0cf0127181498c70e84982fe8980af3dab318abd
dmoser <david.moser@bitmovin.com>
2018-08-21 08:19:10 +0200

    Removed doc string

--- src/main/java/com/bitmovin/api/encoding/manifest/smooth/SmoothStreamingRepresentation.java
+++ src/main/java/com/bitmovin/api/encoding/manifest/smooth/SmoothStreamingRepresentation.java
@@ -2,9 +2,6 @@ package com.bitmovin.api.encoding.manifest.smooth;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-/**
- * Created by atrattnig on 11/16/16.
- */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SmoothStreamingRepresentation
 {
