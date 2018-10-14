f39c6d3f7727684b2173883abf687523ccddfc7d
Roland Kersche <roland.kersche@bitmovin.com>
2018-10-14 11:36:24 +0200

    Added restart of live encodings

---
+++ src/main/java/com/bitmovin/api/encoding/encodings/live/RestartLiveEncodingResponse.java
@@ -0,0 +1,22 @@
+package com.bitmovin.api.encoding.encodings.live;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Created by dmoser on 19.09.16.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RestartLiveEncodingResponse
+{
+    private String id;
+
+    public String getId()
+    {
+        return this.id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+}
