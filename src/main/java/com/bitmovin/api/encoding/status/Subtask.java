eb909b21706381ba631c617c6212014e94638aaf
jonathanperry <jonathan.perry@bitmovin.com>
2018-11-06 10:06:02 -0800

    Expose additional Status properties

--- src/main/java/com/bitmovin/api/encoding/status/Subtask.java
+++ src/main/java/com/bitmovin/api/encoding/status/Subtask.java
@@ -3,6 +3,7 @@ package com.bitmovin.api.encoding.status;
 import com.bitmovin.api.enums.Status;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -18,7 +19,12 @@ public class Subtask
 
     private String name;
 
+    private Date runningAt;
+    private Date updatedAt;
+    private Date finishedAt;
+
     private List<Message> messages;
+    private List<Metadata> metadata;
 
     public Subtask() {}
 
@@ -40,4 +46,20 @@ public class Subtask
     public List<Message> getMessages() { return this.messages; }
 
     public void setMessages(List<Message> messages) { this.messages = messages; }
+
+    public List<Metadata> getMetadata() { return this.metadata; }
+
+    public void setMetadata(List<Metadata> metadata) { this.metadata = metadata; }
+
+    public Date getRunningAt() { return this.runningAt; }
+
+    public void setRunningAt(Date date) { this.runningAt = date; }
+
+    public Date getUpdatedAt() { return this.updatedAt; }
+
+    public void setUpdatedAt(Date date) { this.updatedAt = date; }
+
+    public Date getFinishedAt() { return this.finishedAt; }
+
+    public void setFinishedAt(Date date) { this.finishedAt = date; }
 }
