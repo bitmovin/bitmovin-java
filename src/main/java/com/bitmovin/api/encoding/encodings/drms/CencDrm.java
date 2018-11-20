3a49aec440690b7d3308df135765fd812573d3cd
Marco Sussitz <marco.sussitz@bitmovin.at>
2018-11-20 09:14:11 +0100

    fields IvSize and enablePiffCompativility added

--- src/main/java/com/bitmovin/api/encoding/encodings/drms/CencDrm.java
+++ src/main/java/com/bitmovin/api/encoding/encodings/drms/CencDrm.java
@@ -3,6 +3,7 @@ package com.bitmovin.api.encoding.encodings.drms;
 import com.bitmovin.api.encoding.encodings.drms.cencSystems.CencMarlin;
 import com.bitmovin.api.encoding.encodings.drms.cencSystems.CencPlayReady;
 import com.bitmovin.api.encoding.encodings.drms.cencSystems.CencWidevine;
+import com.bitmovin.api.encoding.encodings.drms.enums.IvSize;
 
 public class CencDrm extends Drm
 {
@@ -11,6 +12,8 @@ public class CencDrm extends Drm
     private CencWidevine widevine;
     private CencPlayReady playReady;
     private CencMarlin marlin;
+    private IvSize ivSize;
+    private Boolean enablePiffCompatibility;
 
     public CencPlayReady getPlayReady()
     {
@@ -58,5 +61,25 @@ public class CencDrm extends Drm
     public void setKid(String kid)
     {
         this.kid = kid;
+    }
+
+    public IvSize getIvSize()
+    {
+        return this.ivSize;
+    }
+
+    public void setIvSize(IvSize ivSize)
+    {
+        this.ivSize = ivSize;
+    }
+
+    public Boolean getEnablePiffCompatibility()
+    {
+        return this.enablePiffCompatibility;
+    }
+
+    public void setEnablePiffCompatibility(Boolean enablePiffCompatibility)
+    {
+        this.enablePiffCompatibility = enablePiffCompatibility;
     }
 }
