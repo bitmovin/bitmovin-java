5a6b929f9308229591435ad56af222f40cc52c16
Peter Kanzow <peter.kanzow@bitmovin.com>
2019-04-02 13:42:02 +0200

    'Initial BBC 52 add Adaptive Quantization Mode'

---
+++ src/main/java/com/bitmovin/api/encoding/codecConfigurations/enums/H264AdaptiveQuantizationMode.java
@@ -0,0 +1,13 @@
+package com.bitmovin.api.encoding.codecConfigurations.enums;
+
+/**
+ * Created by pkanzow on 4/2/19.
+ */
+public enum H264AdaptiveQuantizationMode
+{
+    NONE,
+    DISABLED,
+    VARIANCE,
+    AUTO_VARIANCE,
+    AUTO_VARIANCE_DARK_SCENES
+}
